d16a848cd09c70af6e9d13239b1ae00a16e43e26
SimoneZoroddu <lenviur@gmail.com>
2026-02-23 16:15:03 +0100

    add function for create li and append email random

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 const ulEl = document.querySelector('ul')
+const buttonEl = document.querySelector('button')
 //console.log(divEl);
 
-for (let i = 0; i < 10; i++) {
-    fetch(endpoint)
-        .then(res => res.json())
-        .then(data => {
-            emailRandom = data.response
-            liEl = document.createElement('li')
-            liEl.append(emailRandom)
-            ulEl.appendChild(liEl)
-        }
-    )
+
+
+function restartRandomEmail() {
+    for (let i = 0; i < 10; i++) {
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(data => {
+                emailRandom = data.response
+                liEl = document.createElement('li')
+                liEl.append(emailRandom)
+                ulEl.appendChild(liEl)
+            }
+            )
+    }
 }
+
+restartRandomEmail()
