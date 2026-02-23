11682c33c8be3bf0d7d7fc626700c42f67f3a838
SimoneZoroddu <lenviur@gmail.com>
2026-02-23 16:34:48 +0100

    adding some bootstrap and css

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 const ulEl = document.querySelector('ul')
 const buttonEl = document.querySelector('button')
 //console.log(divEl);
-
-
+/* ulEl.classList.add('list-group') */
 
 function restartRandomEmail() {
     for (let i = 0; i < 10; i++) {
         fetch(endpoint)
-            .then(res => res.json())
-            .then(data => {
-                emailRandom = data.response
-                liEl = document.createElement('li')
-                liEl.append(emailRandom)
-                ulEl.appendChild(liEl)
-            }
-            )
-    }
+        .then(res => res.json())
+        .then(data => {
+            emailRandom = data.response
+            liEl = document.createElement('li')
+            liEl.innerText = 'Email: '
+            liEl.append(emailRandom)
+            ulEl.appendChild(liEl)
+            liEl.classList.add('list-group-item', 'list-group-item-primary', 'm-2', 'w-25', 'mx-auto')
+        }
+    )
+}
 }
 
 restartRandomEmail()
@@ -25,3 +26,4 @@ buttonEl.addEventListener('click', function(){
     ulEl.innerHTML = ''
     restartRandomEmail()
 })
+
